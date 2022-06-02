a0f604faa65c5b7428c67ee8bddc486b09e2c824
vasupradharamachandran <vasupradharamachandrans@gmail.com>
2022-06-02 23:50:35 +0530

    Function to generate hash value

---
+++ ApiFunctions.py
@@ -0,0 +1,28 @@
+# using global variables for testing
+# we can easily implement storeCreds as a DB function
+
+publicKey = None
+privateKey = None
+baseUrl = "http://gateway.marvel.com/v1/public"
+
+def storeCreds(prKey, puKey):
+    global privateKey, publicKey
+    privateKey = prKey
+    publicKey = puKey
+
+
+import hashlib
+from datetime import datetime
+import time
+import requests_cache
+
+session = requests_cache.CachedSession('cacheData')
+
+class Provider:
+
+    def generateHashManual(self, puKey, prKey, ts):
+
+        stringToHash = str(ts) + prKey + puKey
+        reqHash = hashlib.md5(stringToHash.encode()).hexdigest()
+        params = {'ts': str(ts), 'apikey': str(publicKey), 'hash': str(reqHash)}
+        return params
