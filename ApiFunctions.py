d674d487ffbac879ba6201f39066d04c887f6cd3
vasupradharamachandran <vasupradharamachandrans@gmail.com>
2022-06-03 00:34:28 +0530

    Complete dunction to fetch API data

--- ApiFunctions.py
+++ ApiFunctions.py
@@ -21,7 +21,6 @@ session = requests_cache.CachedSession('cacheData')
 class Provider:
 
     def generateHashManual(self, puKey, prKey, ts):
-
         stringToHash = str(ts) + prKey + puKey
         reqHash = hashlib.md5(stringToHash.encode()).hexdigest()
         params = {'ts': str(ts), 'apikey': str(publicKey), 'hash': str(reqHash)}
@@ -30,3 +29,29 @@ class Provider:
     def generateHash(self):
         ts = datetime.now().time()
         return self.generateHashManual(puKey=publicKey, prKey= privateKey, ts=ts)
+
+    def fetchData(self, url, extraparams):
+        params = self.generateHash()
+        params.update(extraparams)
+        resp = session.get(baseUrl+url, params=params)
+        if resp.status_code == 200:
+            return resp.json()
+        else:
+            print("fetchError: API fetch request failed with code" + str(resp.status_code))
+            return None
+
+
+    def getCharacters(self, charName, limit =100):
+        stTime = time.process_time()
+        data = self.fetchData("/characters", extraparams={'limit': limit, 'nameStartsWith': charName})
+        print("Found in "+ str(time.process_time()-stTime))
+        
+        if data != None:
+            return data['data']['results']
+        else:
+            return []
+
+    def registerCreds(self, prKey, puKey):
+        storeCreds(prKey, puKey)
+
+provider = Provider()
