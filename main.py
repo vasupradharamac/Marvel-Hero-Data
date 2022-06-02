2bda9e444eca0bbc10d634f743683d42b2ef2d67
vasupradharamachandran <vasupradharamachandrans@gmail.com>
2022-06-03 00:44:24 +0530

    detecting keyboard actions

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-# using global variables for testing
-# we can easily implement storeCreds as a DB function
+import os
+from ApiFunctions import *
+from pynput import keyboard
 
-publicKey = 
+publicKey = "e04fd41c572564f6ffe36dbfe1173aa3"
+privateKey = "1c5e17de2e5c9f28d7559cd35541c6c6b8a30a02"
+baseUrl = "http://gateway.marvel.com/v1/public"
+
+def detectPress():
+    with keyboard.Events() as events:
+        event = events.get(10**6)
+        return str(event.key)[1]
+
+        
         
