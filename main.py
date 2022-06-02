780e5fc17542a748251ec7d230ee83e93d819bc5
vasupradharamachandran <vasupradharamachandrans@gmail.com>
2022-06-03 00:56:55 +0530

    Implemented pagination

--- main.py
+++ main.py
@@ -11,5 +11,28 @@ def detectPress():
         event = events.get(10**6)
         return str(event.key)[1]
 
-        
-        
+def doPagination(data, itemsPerPage=10):
+    notClosed = True
+    offset = 0
+    maxEntries = len(data)
+    while notClosed and maxEntries>0:
+        for i in range(offset, min(offset+itemsPerPage, maxEntries)):
+            hero = data[i]
+            print("Name :" +hero['name'])
+            if hero.get('thumbnail'):
+                print("ImageUrl :" +hero['thumbnail']['path'])
+            if hero.get('description'):
+                if hero['description']=="":
+                    hero['description'] = "No short description"
+                print("Description: " +hero['description'][:50])
+            print("*"*10)
+        print("X -> Close results, N -> Next page, P -> Previous page")
+        choice = detectPress()
+
+        if choice == "x":
+            notClosed=False
+        elif choice == "n":
+            offset = min(offset+itemsPerPage, maxEntries-itemsPerPage)
+        elif choice == "p":
+            offset = max(offset-itemsPerPage, 0)
+         
