e14527bbea514acced76c54f58fff2c8a7881b44
vasupradharamachandran <vasupradharamachandrans@gmail.com>
2022-06-03 13:08:50 +0530

    Added API keys in a separate file

---
+++ apiKeys.py
@@ -0,0 +1,2 @@
+publicKey = "e04fd41c572564f6ffe36dbfe1173aa3"
+privateKey = "1c5e17de2e5c9f28d7559cd35541c6c6b8a30a02"
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import os
 from ApiFunctions import *
 from pynput import keyboard
+from apiKeys import *
 
-publicKey = "e04fd41c572564f6ffe36dbfe1173aa3"
-privateKey = "1c5e17de2e5c9f28d7559cd35541c6c6b8a30a02"
+publicKey = publicKey
+privateKey = privateKey
 baseUrl = "http://gateway.marvel.com/v1/public"
 
 def detectPress():
